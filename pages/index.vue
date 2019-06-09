504cf5dbc31c150cd2878618b67afa11fc7b6cab
jrah <jjames.home@gmail.com>
2019-06-09 17:57:45 +0100

    Update pages index

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,39 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        britcentprismic
-      </h1>
-      <h2 class="subtitle">
-        Britcent Project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
-      </div>
-    </div>
+  <div>
+    <f-main-header
+      :title="document.data.header_title"
+      :background-url="document.data.header_image.url"
+    ></f-main-header>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+// fixed
+import fMainHeader from '~/components/fixed/MainHeader.vue'
+// import fForm from '~/components/fixed/Form.vue'
 
+import { PrismicMixin } from '~/assets/js/getPrismicData.js'
 export default {
   components: {
-    Logo
-  }
+    fMainHeader
+  },
+  mixins: [PrismicMixin],
+  // data() {
+  //   return {
+  //     title: this.$prismic.asText(document.data.header_title)
+  //   };
+  // },
+  data() {
+    return {
+      pageType: 'index'
+    }
+  },
+  computed: {}
 }
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+<style lang="css">
+.mainHeader h1 {
+  @apply text-white;
 }
 </style>
