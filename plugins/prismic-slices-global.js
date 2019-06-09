2de1f3e9cb667cc9fb57a3960e8b7055a1573024
jrah <jjames.home@gmail.com>
2019-06-09 17:48:33 +0100

    Add testimonial slice to global slices

--- plugins/prismic-slices-global.js
+++ plugins/prismic-slices-global.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
+import prismicTestimonial from '~/components/slices/Testimonial.vue'
 
-const components = {}
+const components = {
+  prismicTestimonial
+}
 
 Object.entries(components).forEach(([name, component]) => {
   Vue.component(name, component)
